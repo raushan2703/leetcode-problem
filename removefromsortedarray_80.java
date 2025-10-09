636b6320266ad5a944f5acbe92ff6ecc2363c658
RAUSHAN KUMAR <98824287+raushan2703@users.noreply.github.com>
2025-10-09 20:21:11 +0530

    Update removefromsortedarray_80.java

--- removefromsortedarray_80.java
+++ removefromsortedarray_80.java
@@ -1,17 +1,12 @@
-/*Input: nums = [1,1,1,2,2,3]
-Output: 5, nums = [1,1,2,2,3,_]
-  */
 class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 1;
-        for (int i = 1; i<nums.length; i++){
-            if ( j== 1 || nums[i] != nums[j-2]){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[j] = nums[i];
                 j++;
-
             }
         }
         return j;
-        
     }
 }
